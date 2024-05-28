3f7a5664a44ad8ac85e779e8e58dbfc730d458e2
Mitesh Banka <mitesh@frrole.com>
2024-05-29 00:30:17 +0530

    initial commit

---
+++ src/components/Auth/Signup.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+  const [formData, setFormData] = useState({ username: '', email: '', password: '' });
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:5001/api/auth/signup', formData);
+      setMessage(res.data.message);
+    } catch (err) {
+      setMessage(err.response.data.error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Username" onChange={handleChange} />
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+        <button type="submit">Signup</button>
+      </form>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Signup;
